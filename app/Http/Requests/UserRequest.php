2e2826c0941b14be62509c4800f256732d8ff81c
Chen zhulin <2655310177@qq.com>
2019-05-07 10:23:40 +0800

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -27,11 +27,14 @@ class UserRequest extends FormRequest
             'name'=>'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,'.Auth::id(),
             'email'=>'required|email',
             'introduction'=>'max:80',
+            'avatar'=>'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208',
         ];
     }
     public function messages()
     {
         return [
+            'avatar.mimes'=>'头像必须是jpeg,bmp,png,gif 格式的图片',
+            'avatar.dimensions'=>'图片的清晰度不够，宽高必须是 208px 以上',
             'name.unique'=>'用户名已被占用，请重新填写',
             'name.regex'=>'用户名只支持英文、数字、下划线和横杠',
             'name.between'=>'用户名必须是3-25个字符之间',
