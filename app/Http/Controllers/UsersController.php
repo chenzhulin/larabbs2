cf7718938405d2956043c27975f00ee76a41130e
Chen zhulin <2655310177@qq.com>
2019-05-07 10:14:35 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
     public function show(User $user)
@@ -12,9 +13,17 @@ class UsersController extends Controller
         return view('users.show',compact('user'));
     }
 
-    public function update(UserRequest  $request,User $user)
+    public function update(UserRequest  $request,User $user,ImageUploadHandler $uploader)
     {
-        $user->update($request->all());
+        $data = $request->all();
+
+        if ($request->avatar){
+            $result = $uploader->save($request->avatar,'avatars',$user->id);
+            if ($result){
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
     }
 
