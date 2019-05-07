b6a8d84f78283cc12aea92f66306ae0012480e1d
Chen zhulin <2655310177@qq.com>
2019-05-07 09:46:18 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use App\Http\Requests\UserRequest;
 class UsersController extends Controller
 {
     public function show(User $user)
@@ -12,13 +12,14 @@ class UsersController extends Controller
         return view('users.show',compact('user'));
     }
 
-    public function update(Request $request,User $user)
+    public function update(UserRequest  $request,User $user)
     {
-
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
     }
 
     public function edit(User $user)
     {
-
+        return view('users.edit',compact('user'));
     }
 }
