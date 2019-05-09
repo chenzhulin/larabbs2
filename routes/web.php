1601d92758695a75594bc6cd649f966ca69d0ef6
Chen zhulin <2655310177@qq.com>
2019-05-09 11:55:56 +0800

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,5 @@ Route::resource('replies', 'RepliesController', ['only' => [ 'store','destroy']]
 
 //通知列表
 Route::resource('notifications','NotificationsController',['only'=>['index']]);
+
+Route::get('permission-denied','PagesController@permissionDenied')->name('permission-denied');
